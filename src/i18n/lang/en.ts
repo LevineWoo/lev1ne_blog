a3ce8759c825a7e5f533861b5d6b1f860fed717b
LevineWoo <gd.wulw@gmail.com>
2026-07-23 00:10:01 +0800

    改用香港 Geek 風介面文字

--- src/i18n/lang/en.ts
+++ src/i18n/lang/en.ts
@@ -2,70 +2,82 @@ import type { UIStrings } from "../types";
 
 export default {
   nav: {
-    home: "主页",
+    home: "首頁",
     posts: "文章",
-    tags: "标签",
-    about: "关于",
-    archives: "归档",
-    search: "搜索",
+    tags: "標籤",
+    about: "關於",
+    archives: "時光機",
+    search: "想搵咩？",
   },
+
   post: {
-    publishedAt: "Published at",
-    updatedAt: "Updated",
-    sharePostIntro: "Share this post:",
-    sharePostOn: "Share this post on {{platform}}",
-    sharePostViaEmail: "Share this post via email",
-    tagLabel: "Tags",
-    backToTop: "Back to top",
-    goBack: "Go back",
-    editPage: "Edit page",
-    previousPost: "Previous Post",
-    nextPost: "Next Post",
+    publishedAt: "發佈於",
+    updatedAt: "更新日期",
+    sharePostIntro: "Share 哩篇文：",
+    sharePostOn: "Share 去 {{platform}}",
+    sharePostViaEmail: "用 Email Share",
+    tagLabel: "標籤",
+    backToTop: "返頂",
+    goBack: "返上一頁",
+    editPage: "編輯哩篇文",
+    previousPost: "上一篇文",
+    nextPost: "下一篇文",
   },
+
   pagination: {
-    prev: "Prev",
-    next: "Next",
-    page: "Page",
+    prev: "上一頁",
+    next: "下一頁",
+    page: "第 {{page}} 頁",
   },
+
   home: {
-    socialLinks: "社交链接",
-    featured: "精选文章",
+    socialLinks: "搵我",
+    featured: "推介文章",
     recentPosts: "最新文章",
-    allPosts: "全部文章",
+    allPosts: "睇晒全部文章",
   },
+
   footer: {
     copyright: "Copyright",
-    allRightsReserved: "All rights reserved.",
+    allRightsReserved: "Made with Astro • Hosted on Cloudflare Pages",
   },
+
   pages: {
-    tagTitle: "Tag",
-    tagDesc: "All the articles with the tag",
+    tagTitle: "標籤",
 
-    tagsTitle: "Tags",
-    tagsDesc: "All the tags used in posts.",
+    tagDesc: "所有用咗呢個標籤嘅文章。",
 
-    postsTitle: "Posts",
-    postsDesc: "All the articles I've posted.",
+    tagsTitle: "所有標籤",
 
-    archivesTitle: "Archives",
-    archivesDesc: "All the articles I've archived.",
+    tagsDesc: "整理所有文章使用過的標籤。",
 
-    searchTitle: "Search",
-    searchDesc: "Search any article ...",
+    postsTitle: "所有文章",
+
+    postsDesc: "這裡收錄了全部技術文章與折騰記錄。",
+
+    archivesTitle: "時光機",
+
+    archivesDesc: "舊文全部擺晒喺哩度。",
+
+    searchTitle: "搜尋",
+
+    searchDesc: "輸入關鍵字，睇下有冇寫過。",
   },
+
   a11y: {
-    skipToContent: "Skip to content",
-    openMenu: "Open menu",
-    closeMenu: "Close menu",
-    toggleTheme: "Toggle theme",
-    searchPlaceholder: "Search posts...",
-    noResults: "No results found",
-    goToPreviousPage: "Go to previous page",
-    goToNextPage: "Go to next page",
+    skipToContent: "跳去主要內容",
+    openMenu: "打開選單",
+    closeMenu: "收起選單",
+    toggleTheme: "轉深色／淺色模式",
+    searchPlaceholder: "想搵咩？",
+    noResults: "搵唔到相關內容 🤷",
+    goToPreviousPage: "去上一頁",
+    goToNextPage: "去下一頁",
   },
+
   notFound: {
-    title: "404 Not Found",
-    message: "Page Not Found",
-    goHome: "Go back home",
+    title: "404",
+    message: "So9ry，哩頁唔知去咗邊 🤷",
+    goHome: "返首頁",
   },
 } satisfies UIStrings;
